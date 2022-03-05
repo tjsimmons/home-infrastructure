d2849b874d38ad33986767a8ec810c620ddf5b7e
T.J. Simmons <tj@tjsimmons.net>
2022-03-04 21:45:02 -0600

    got every piece of infrastructure imported and written up

--- modules/lxc/main.tf
+++ modules/lxc/main.tf
@@ -13,7 +13,6 @@ resource "proxmox_lxc" "atlas" {
   arch         = "amd64"
   unprivileged = false
   onboot       = true
-  vmid         = 101
   cores        = 1
   memory       = 1024
   swap         = 1024
@@ -37,10 +36,8 @@ resource "proxmox_lxc" "sonarr" {
   hostname     = "sonarr"
   unprivileged = false
   onboot       = true
-  startup      = "order=11,up=60"
   ostype       = "ubuntu"
   arch         = "amd64"
-  vmid         = 102
   cores        = 2
   memory       = 2048
   swap         = 2048
@@ -74,7 +71,6 @@ resource "proxmox_lxc" "apt-repo" {
   onboot       = true
   ostype       = "ubuntu"
   arch         = "amd64"
-  vmid         = 103
   cores        = 2
   memory       = 1024
   swap         = 1024
@@ -90,5 +86,115 @@ resource "proxmox_lxc" "apt-repo" {
     bridge = "vmbr2"
     ip     = "dhcp"
     hwaddr = "5E:C2:E3:A3:27:4C"
+  }
+}
+
+resource "proxmox_lxc" "pihole" {
+  target_node  = "bragr"
+  hostname     = "pihole"
+  unprivileged = false
+  onboot       = true
+  startup      = "order=0,up=60"
+  ostype       = "ubuntu"
+  arch         = "amd64"
+  cores        = 4
+  memory       = 4096
+  swap         = 4096
+  rootfs {
+    storage = "local-lvm-data-hdd"
+    size    = "10G"
+  }
+
+  network {
+    name   = "eth0"
+    bridge = "vmbr2"
+    ip     = "10.0.1.50/24"
+    gw     = "10.0.1.1"
+    hwaddr = "BA:7E:67:C4:49:DA"
+  }
+}
+
+resource "proxmox_lxc" "nextcloud" {
+  target_node  = "odin"
+  hostname     = "nextcloud"
+  unprivileged = false
+  onboot       = true
+  ostype       = "ubuntu"
+  arch         = "amd64"
+  cores        = 4
+  memory       = 2048
+  swap         = 2048
+  rootfs {
+    storage = "local-lvm-data-hdd"
+    size    = "20G"
+  }
+
+  mountpoint {
+    mp      = "/var/www/nextcloud/data"
+    size    = "500G"
+    storage = "local-lvm-data-hdd"
+    slot    = "0"
+    key     = 0
+    // Without 'volume' defined, Proxmox will try to create a volume with
+    // the value of 'storage' + : + 'size' (without the trailing G) - e.g.
+    // "/srv/host/bind-mount-point:256".
+    // This behaviour looks to be caused by a bug in the provider.
+    volume = "local-lvm-data-hdd:vm-106-disk-1"
+  }
+
+  network {
+    name   = "eth0"
+    bridge = "vmbr2"
+    ip     = "dhcp"
+    hwaddr = "7A:C6:16:0C:39:C7"
+  }
+}
+
+resource "proxmox_lxc" "plex" {
+  target_node  = "odin"
+  hostname     = "plex"
+  unprivileged = true
+  onboot       = true
+  ostype       = "ubuntu"
+  arch         = "amd64"
+  cores        = 4
+  memory       = 8192
+  swap         = 8192
+  rootfs {
+    storage = "local-lvm-data-ssd"
+    size    = "20G"
+  }
+
+  mountpoint {
+    mp      = "/usr/media"
+    size    = "5000G"
+    storage = "local-lvm-data-hdd"
+    slot    = "0"
+    key     = 0
+    // Without 'volume' defined, Proxmox will try to create a volume with
+    // the value of 'storage' + : + 'size' (without the trailing G) - e.g.
+    // "/srv/host/bind-mount-point:256".
+    // This behaviour looks to be caused by a bug in the provider.
+    volume = "local-lvm-data-hdd:vm-109-disk-0"
+  }
+
+  mountpoint {
+    mp      = "/usr/media/dagr"
+    size    = "1000G"
+    storage = "dagr-nfs-proxmox"
+    slot    = "1"
+    key     = 1
+    // Without 'volume' defined, Proxmox will try to create a volume with
+    // the value of 'storage' + : + 'size' (without the trailing G) - e.g.
+    // "/srv/host/bind-mount-point:256".
+    // This behaviour looks to be caused by a bug in the provider.
+    volume = "dagr-nfs-proxmox:109/vm-109-disk-0.raw"
+  }
+
+  network {
+    name   = "eth0"
+    bridge = "vmbr2"
+    ip     = "dhcp"
+    hwaddr = "C6:0C:17:3C:C9:F5"
   }
 }
--- modules/vm_qemu/main.tf
+++ modules/vm_qemu/main.tf
@@ -33,3 +33,71 @@ resource "proxmox_vm_qemu" "minecraft" {
     size    = "32G"
   }
 }
+
+resource "proxmox_vm_qemu" "homeassistant" {
+  full_clone  = false
+  agent       = 1
+  boot        = "order=scsi0;ide2;net0"
+  name        = "homeassistant"
+  target_node = "thor"
+  onboot      = true
+  memory      = 2048
+  balloon     = 1
+  sockets     = 1
+  cores       = 2
+  scsihw      = "virtio-scsi-pci"
+
+  network {
+    model    = "virtio"
+    macaddr  = "EE:70:AE:D2:00:A1"
+    bridge   = "vmbr2"
+    firewall = true
+  }
+
+  disk {
+    type    = "scsi"
+    storage = "local-lvm-data-hdd"
+    backup  = 1
+    size    = "40G"
+  }
+
+  usb {
+    host = "0658:0200"
+  }
+}
+
+resource "proxmox_vm_qemu" "untangle" {
+  full_clone  = false
+  agent       = 1
+  boot        = "order=scsi0;net0"
+  name        = "untangle"
+  target_node = "bragr"
+  onboot      = true
+  cpu         = "kvm64"
+  memory      = 8192
+  balloon     = 0
+  sockets     = 1
+  cores       = 4
+  scsihw      = "virtio-scsi-pci"
+
+  network {
+    model    = "virtio"
+    macaddr  = "16:55:1D:BD:4B:0D"
+    bridge   = "vmbr1"
+    firewall = false
+  }
+
+  network {
+    model    = "virtio"
+    macaddr  = "72:9E:29:6C:6A:EF"
+    bridge   = "vmbr2"
+    firewall = false
+  }
+
+  disk {
+    type    = "scsi"
+    storage = "local-lvm-data-hdd"
+    backup  = 1
+    size    = "120G"
+  }
+}
